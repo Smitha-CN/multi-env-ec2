70c688e03beba922b1fc3d139ea9ce41c0aa3389
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 22:39:27 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "aws_region" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "key_name" {}
+variable "security_group_ids" {
+  type = list(string)
+}
+variable "environment" {}
 
