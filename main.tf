9ed1d4e7d1936e5a0db4e6fbd018c0231ecf3934
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 22:39:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-mai.tf
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = {
+    Name = "${var.environment}-ec2"
+  }
+}
+
